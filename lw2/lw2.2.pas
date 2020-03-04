62548824d3e67ea90080d83c8dce5cfb76f7451e
Daniil <dr.chashka@gmail.com>
2020-03-04 19:33:03 +0300

    Second lab

---
+++ lw2/lw2.2.pas
@@ -0,0 +1,30 @@
+PROGRAM SarahRevere(INPUT, OUTPUT);
+USES
+  DOS;
+VAR
+  Query: STRING;
+  Lanterns: CHAR;
+  I: INTEGER;
+BEGIN {PaulRevere}
+  WRITELN('Content-Type: text/plain');
+  WRITELN;
+  Query := GetEnv('QUERY_STRING');
+  FOR I := 1 TO Length(Query)
+  DO
+    BEGIN
+      IF Query[I] = '='
+      THEN
+        Lanterns := Query[I + 1];
+    END;
+  IF (Lanterns > '0') AND (Lanterns < '3')
+  THEN
+    WRITE('The British are coming by ');
+    IF Lanterns = '1'
+    THEN
+      WRITELN('land.')
+    ELSE IF Lanterns = '2'
+    THEN
+      WRITELN('sea.')
+  ELSE
+    WRITELN('The North Church shows only ''', Lanterns, '''.')
+END. {PaulRevere}
