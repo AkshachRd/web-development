02d142ad5b305b60aba794372ea07a3a622e57af
Daniil <dr.chashka@gmail.com>
2020-03-05 09:33:27 +0300

    Useless files have been deleted

---
+++ lw3/task4.php
@@ -0,0 +1,42 @@
+<?php
+header("Content-Type: text/plain");
+$inputData = array(
+	'first_name' => getQueryStringParameter('first_name'),
+	'last_name' => getQueryStringParameter('last_name'),
+	'email' => getQueryStringParameter('email'),
+	'age' => getQueryStringParameter('age'),
+);
+function surveySaver(string $data): void
+{
+	$resultString = '';
+
+	if (($data['first_name'] !== null) && ($data['first_name'] !== ''))
+	{
+		$resultString .= 'First Name: ';
+		$resultString .= $data['first_name'];
+		$resultString .= "\n";
+	}
+	if (($data['last_name'] !== null) && ($data['last_name'] !== ''))
+	{
+		$resultString .= 'Last Name: ';
+		$resultString .= $data['last_name'];
+		$resultString .= "\n";
+	}
+	$resultString .= 'Email: ';
+	$resultString .= $data['email'];
+	$resultString .= "\n";
+	if (($data['age'] !== null) && ($data['age'] !== ''))
+	{
+		$resultString .= 'Age: ';
+		$resultString .= $data['age'];
+	}
+	$filename = './data/';
+	$filename .= $data['email'];
+	$filename .= '.txt';
+	file_put_contents($filename, $resultString);
+}
+
+function getQueryStringParameter(string $name): ?string
+{
+   return isset($_GET[$name]) ? $_GET[$name] : null;
+}
