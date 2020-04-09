7d2ef4bbc9897d0c02521f6aee4e5ddf52e73341
Daniil <dr.chashka@gmail.com>
2020-04-09 08:07:38 +0300

    The code became more beautiful

--- lw7/Task_1.js
+++ lw7/Task_1.js
@@ -1,33 +1,33 @@
 function isPrimeNumber(n) {
-  let isPrime
-  let exit = false
+  let isPrime;
+  let exit = false;
+  //
   if (typeof n === 'number') {
-    n = [n]
-  }
-  else {
+    n = [n];
+  } else {
     for (let k in n) {
       if (typeof n[k] !== 'number') {
-        console.log('Error: incorrect type of array\'s element')
-        exit = true
+        console.log('Error: incorrect type of array\'s element');
+        exit = true;
       }
     }
   }
+  //
   if (!exit) { 
     for (let i in n) {
-      isPrime = true
+      isPrime = true;
       for (let j = 2; j < n[i]; j++) {
         if (n[i] % j == 0) {
-          isPrime = false
-          break
+          isPrime = false;
+          break;
         }
       }
       if (isPrime) {
-        console.log(n[i] + ' is prime number')
-      }
-      else {
-        console.log(n[i] + ' is not prime number')
+        console.log(n[i] + ' is prime number');
+      } else {
+        console.log(n[i] + ' is not prime number');
       }
     }
   }
 }
-isPrimeNumber(5)
+isPrimeNumber(5);
