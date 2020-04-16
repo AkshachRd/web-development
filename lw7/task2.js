055777eda247cd42a1a1321584146ba7632ed6ff
Daniil <dr.chashka@gmail.com>
2020-04-17 00:50:46 +0300

    Fixed brackets error message error

--- lw7/task2.js
+++ lw7/task2.js
@@ -23,28 +23,29 @@ function findNum(l, n, s) {
   return [l, num];
 }
 
-function findBrackets(l, n, s) {
+function findBrackets(l, n, s, error) {
   let brackestResult;
   let lAndVar;
   
   while (l < n) {
-    lAndVar = answ(l, n, s);
+    lAndVar = answ(l, n, s, error);
     l = lAndVar[0];
     brackestResult = lAndVar[1];
+    error = lAndVar[2];
     if (s[l] === ')') {
       break;
     }
     l++;
   }
-  if (l === n) {
+  if (l > n) {
     console.log('Error: closing bracket wasn\'t found');
+    error = true;
   }
-  return [l, brackestResult];
+  return [l, brackestResult, error];
 }
 
 function findSigh(l, n, s) {
   let sigh;
-  let lAndVar;
   
   while (l < n) {
     if (
@@ -62,7 +63,39 @@ function findSigh(l, n, s) {
   return [l, sigh];
 }
 
-function answ(l, n, s) {
+function findBracketsOrNum(l, n, s, error) {
+  let lAndVar;
+
+  while (l < n) {
+    if (s[l] === '('){
+      lAndVar = findBrackets(l, n, s, error);
+      l = lAndVar[0];
+      a = lAndVar[1];
+      error = lAndVar[2];
+      break;
+    } else if (
+      s[l] === '0' ||
+      s[l] === '1' ||
+      s[l] === '2' ||
+      s[l] === '3' ||
+      s[l] === '4' ||
+      s[l] === '5' ||
+      s[l] === '6' ||
+      s[l] === '7' ||
+      s[l] === '8' ||
+      s[l] === '9'
+    ) {
+      lAndVar = findNum(l, n, s);
+      l = lAndVar[0];
+      a = lAndVar[1];
+      break;
+    }
+    l++;
+  }
+  return [l, a, error];
+}
+
+function answ(l, n, s, error) {
   let a, b;
   let sigh;
   let lAndVar;
@@ -73,143 +106,69 @@ function answ(l, n, s) {
     sigh = lAndVar[1];
     //*********************************************************
     if (sigh === '*') {
-      while (l < n) {
-        if (s[l] === '('){
-          lAndVar = findBrackets(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        } else if (
-          s[l] === '0' ||
-          s[l] === '1' ||
-          s[l] === '2' ||
-          s[l] === '3' ||
-          s[l] === '4' ||
-          s[l] === '5' ||
-          s[l] === '6' ||
-          s[l] === '7' ||
-          s[l] === '8' ||
-          s[l] === '9'
-        ) {
-          lAndVar = findNum(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        }
-        l++;
-      }
-      lAndVar = findNum(l, n, s);
+      lAndVar = findBracketsOrNum(l, n, s, error);
+      l = lAndVar[0];
+      a = lAndVar[1];
+      error = lAndVar[2];
+      lAndVar = findBracketsOrNum(l, n, s, error);
       l = lAndVar[0];
       b = lAndVar[1];
+      error = lAndVar[2];
       a = b * a;
     }
     ///////////////////////////////////////////////////////////
     if (sigh === '/') {
-      while (l < n) {
-        if (s[l] === '('){
-          lAndVar = findBrackets(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        } else if (
-          s[l] === '0' ||
-          s[l] === '1' ||
-          s[l] === '2' ||
-          s[l] === '3' ||
-          s[l] === '4' ||
-          s[l] === '5' ||
-          s[l] === '6' ||
-          s[l] === '7' ||
-          s[l] === '8' ||
-          s[l] === '9'
-        ) {
-          lAndVar = findNum(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        }
-        l++;
-      }
-      lAndVar = findNum(l, n, s);
+      lAndVar = findBracketsOrNum(l, n, s, error);
+      l = lAndVar[0];
+      a = lAndVar[1];
+      error = lAndVar[2];
+      lAndVar = findBracketsOrNum(l, n, s, error);
       l = lAndVar[0];
       b = lAndVar[1];
-      a = a / b;    
+      error = lAndVar[2];
+      a = a / b;
     }
     //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     if (sigh === '+') {
-      while (l < n) {
-        if (s[l] === '('){
-          lAndVar = findBrackets(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        } else if (
-          s[l] === '0' ||
-          s[l] === '1' ||
-          s[l] === '2' ||
-          s[l] === '3' ||
-          s[l] === '4' ||
-          s[l] === '5' ||
-          s[l] === '6' ||
-          s[l] === '7' ||
-          s[l] === '8' ||
-          s[l] === '9'
-        ) {
-          lAndVar = findNum(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        }
-        l++;
-      }
-      lAndVar = findNum(l, n, s);
+      lAndVar = findBracketsOrNum(l, n, s, error);
+      l = lAndVar[0];
+      a = lAndVar[1];
+      error = lAndVar[2];
+      lAndVar = findBracketsOrNum(l, n, s, error);
       l = lAndVar[0];
       b = lAndVar[1];
+      error = lAndVar[2];
       a = Number(b) + Number(a);     
     }
     //---------------------------------------------------------
     if (sigh === '-') {
-      while (l < n) {
-        if (s[l] === '('){
-          lAndVar = findBrackets(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        } else if (
-          s[l] === '0' ||
-          s[l] === '1' ||
-          s[l] === '2' ||
-          s[l] === '3' ||
-          s[l] === '4' ||
-          s[l] === '5' ||
-          s[l] === '6' ||
-          s[l] === '7' ||
-          s[l] === '8' ||
-          s[l] === '9'
-        ) {
-          lAndVar = findNum(l, n, s);
-          l = lAndVar[0];
-          a = lAndVar[1];
-          break;
-        }
-        l++;
-      }
-      lAndVar = findNum(l, n, s);
+      lAndVar = findBracketsOrNum(l, n, s, error);
+      l = lAndVar[0];
+      a = lAndVar[1];
+      error = lAndVar[2];
+      lAndVar = findBracketsOrNum(l, n, s, error);
       l = lAndVar[0];
       b = lAndVar[1];
+      error = lAndVar[2];
       a = a - b;    
     } else if (a !== undefined) {
       break;
     }
     l++;
   }
-  return [l, a];
+  return [l, a, error];
 }
 
 function calc(str) {
   let n = str.length;
   let l = 0;
+  let lAndVar;
+  let error = false;
   
-  console.log( answ(l, n, str)[1] );
-  return null;
+  //console.log( answ(l, n, str)[1] );
+  lAndVar = answ(l, n, str, error);
+  error = lAndVar[2];
+  if (!error) {
+    return lAndVar[1];;
+  }
 }
