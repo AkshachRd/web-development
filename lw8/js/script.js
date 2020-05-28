7b72c219f8b799969120bd52e4da53a8f69a4dbf
Daniil <dr.chashka@gmail.com>
2020-05-28 08:44:20 +0300

    Variables was renamed

--- lw8/js/script.js
+++ lw8/js/script.js
@@ -1,32 +1,32 @@
 const films = ["first_film", "second_film", "third_film", "fourth_film", "fifth_film", "sixth_film", "seventh_film", "eighth_film", "ninth_film", "tenth_film"]
 
-const left_arrow = document.getElementById('left_arrow');
-left_arrow.onclick = () => scrollFilmsToTheLeft(films);
-const right_arrow = document.getElementById('right_arrow_background');
-right_arrow.onclick = () => scrollFilmsToTheRight(films);
+const leftArrow = document.getElementById('left_arrow');
+leftArrow.onclick = () => scrollFilmsToTheLeft(films);
+const rightArrow = document.getElementById('right_arrow_background');
+rightArrow.onclick = () => scrollFilmsToTheRight(films);
 
 function scrollFilmsToTheLeft(films) {
-  let film_from, film_to;
+  let filmFrom, filmTo;
 
   const tmp = document.getElementById("tenth_film").innerHTML;
   for (let i = films.length - 2; i >= 0; i--) {
-    film_from = document.getElementById(films[i]);
-    film_to = document.getElementById(films[i + 1]);
+    filmFrom = document.getElementById(films[i]);
+    filmTo = document.getElementById(films[i + 1]);
 
-    film_to.innerHTML = film_from.innerHTML;
+    filmTo.innerHTML = filmFrom.innerHTML;
   }
   document.getElementById("first_film").innerHTML = tmp;
 }
 
 function scrollFilmsToTheRight(films) {
-  let film_from, film_to;
+  let filmFrom, filmTo;
 
   const tmp = document.getElementById("first_film").innerHTML;
   for (let i = 1; i <= 9; i++) {
-    film_from = document.getElementById(films[i]);
-    film_to = document.getElementById(films[i - 1]);
+    filmFrom = document.getElementById(films[i]);
+    filmTo = document.getElementById(films[i - 1]);
 
-    film_to.innerHTML = film_from.innerHTML;
+    filmTo.innerHTML = filmFrom.innerHTML;
   }
   document.getElementById("tenth_film").innerHTML = tmp;
 }
